b705e943e746b6861a63b5eab79479911546501a
Dan <9156964+DanTheCoder@users.noreply.github.com>
2023-03-06 18:02:56 -0500

    Added dark mode support to the confirm component

--- src/components/confirm/Main.vue
+++ src/components/confirm/Main.vue
@@ -10,7 +10,7 @@
                 <span class="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">&#8203;</span>
             
                 <TransitionChild as="template" enter="ease-out duration-300" enter-from="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95" enter-to="opacity-100 translate-y-0 sm:scale-100" leave="ease-in duration-200" leave-from="opacity-100 translate-y-0 sm:scale-100" leave-to="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95">
-                    <div class="inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-sm sm:w-full sm:p-6">
+                    <div class="inline-block align-bottom bg-white dark:bg-gray-800 rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-sm sm:w-full sm:p-6">
                         
                         <div>
                             <div class="mx-auto flex items-center justify-center h-16 w-16 rounded-full bg-yellow-100">
@@ -20,8 +20,8 @@
                             </div>
                   
                             <div class="mt-3 text-center sm:mt-5">
-                                <h3 class="text-lg font-medium text-gray-800">{{ title }}</h3>
-                                <p class="text-sm text-gray-500">{{ message }}</p>
+                                <h3 class="text-lg font-medium text-gray-800 dark:text-gray-100">{{ title }}</h3>
+                                <p class="text-sm text-gray-500 dark:text-gray-300">{{ message }}</p>
                             </div>
                         </div>
 
